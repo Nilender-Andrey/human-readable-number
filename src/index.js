061d0184ea07dc6543f22c2259a4d1cb53c7d048
Nilender-Andrey <nilenderav@mail.ru>
2021-03-12 10:20:43 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function toReadable (number) {
+
+  if (number === 0) return 'zero';
+  if (number === 1000) return 'one thousand'
   
+  let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+  'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+  'seventeen', 'eighteen', 'nineteen'];  
+  
+  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+  'ninety']; 
+
+  let numString = number.toString();
+
+  if (number < 20) return ones[number];
+
+  if (numString.length === 2) {
+    if ( numString[1] == 0 ) return (tens[numString[0]]);
+    else return (tens[numString[0]] + ' ' + ones[numString[1]]);
+  }
+  
+
+
+  if (numString.length === 3) {
+    
+    if ( numString[1] == 0 && numString[2] == 0 ) return (ones[numString[0]] + ' ' +'hundred');
+
+    else{
+      let end = number%100; 
+     
+      if (end < 20) 
+      return (ones[numString[0]] + ' hundred' +' '+ ones[end]);
+      
+      if (end >= 20){
+        if ( numString[2] == 0 ) return (ones[numString[0]] + ' ' +'hundred' + ' ' + tens[numString[1]]);
+         
+     return (ones[numString[0]] + ' ' +'hundred' + ' ' + tens[numString[1]] + ' ' + ones[numString[2]]);
+  }
+ }
+}
 }
